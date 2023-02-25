7b28e85b5b593ff7c00c134402ec17520a159e9a
User <nazarovtolya2@gmail.com>
2023-02-25 20:42:16 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,18 @@ public class Main {
     }
 
     public static void task1() {
-        System.out.println("Задача1");
+        System.out.println("Задача 1");
+        int [] number = new int [3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        System.out.println(number[2]);
 
+        double [] number2 = {1.57, 7.654, 9.986};
+        System.out.println(number2[2]);
+
+        double [] randomMassive = {23, 105.54, 646466.465454};
+        System.out.println(randomMassive[0]);
     }
+
 }
