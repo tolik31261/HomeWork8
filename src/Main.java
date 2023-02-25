ab0c60b1f26a325b4bbddb81df5c351f7b534e9b
User <nazarovtolya2@gmail.com>
2023-02-25 22:14:28 +0500

    Финальное выполнение

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -6,32 +8,13 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int [] number = new int [3];
-        number[0] = 1;
-        number[1] = 2;
-        number[2] = 3;
+        int[] number = {1, 2, 3};
         for (int i = 0; i < number.length; i++) {
-            System.out.print(number[i] + ", ");
+            if (i % 2 == 0) {
+                number[i] += 1;
+
+            }
         }
-        System.out.println();
-
-
-        double [] number2 = {1.57, 7.654, 9.986};
-        for (int i = 0; i < number2.length; i++) {
-            System.out.print(number2[i] + ", ");
-        }
-        System.out.println();
-
-        double [] randomMassive = {23, 105.54, 646466.465454};
-        for (int i = 0; i < randomMassive.length; i++) {
-            System.out.print(randomMassive[i] + ", ");
-        }
-        System.out.println();
-
-
-
+        System.out.println(Arrays.toString(number));
     }
-
-
-
 }
