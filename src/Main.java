20110527b016b24bdea9179921432f274314e328
User <nazarovtolya2@gmail.com>
2023-02-25 21:32:36 +0500

    Исправил задачу 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
+
     }
 
     public static void task1() {
@@ -10,26 +10,28 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        System.out.println(number[2]);
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + ", ");
+        }
+        System.out.println();
+
 
         double [] number2 = {1.57, 7.654, 9.986};
-        System.out.println(number2[2]);
+        for (int i = 0; i < number2.length; i++) {
+            System.out.print(number2[i] + ", ");
+        }
+        System.out.println();
 
         double [] randomMassive = {23, 105.54, 646466.465454};
-        System.out.println(randomMassive[0]);
-    }
-
-    public static void task2() {
-        System.out.println("Задача 2");
-        int [] massive = new int [10];
-        for (int index = 0; index < massive.length; index++) {
-            if (index == massive.length - 1) {
-                System.out.println(massive[index]);
-                break;
-            }
-            System.out.println(massive[index]);
+        for (int i = 0; i < randomMassive.length; i++) {
+            System.out.print(randomMassive[i] + ", ");
         }
+        System.out.println();
+
+
 
     }
+
+
 
 }
